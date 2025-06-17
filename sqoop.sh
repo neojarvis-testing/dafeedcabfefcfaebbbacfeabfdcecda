87e8ea2c6ea4d108e8219ad3b92071d05877fcf8
neojarvis <techadmin@iamneo.ai>
2025-06-17 08:05:16 +0000

    Submit-project-save-commit-17/06/2025--13:35:11

--- sqoop.sh
+++ sqoop.sh
@@ -4,7 +4,7 @@ echo "🔍 Checking Sqoop..."
 # Dummy check: test connection to a local MySQL/PostgreSQL DB
 sqoop list-databases \
   --connect jdbc:mysql://localhost:3306/ \
-  --username root --password root \
+  --username root --password examly \
   || echo "❌ Sqoop DB connection failed"
 
 echo "[ℹ️] Sqoop has no Web UI."
