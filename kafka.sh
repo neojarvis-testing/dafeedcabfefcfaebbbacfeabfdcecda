4b068e76d0b314d1a3fa03d0b0fc59b48b6ea75a
neojarvis <techadmin@iamneo.ai>
2025-06-26 09:40:11 +0000

    Auto-save-commit-26/06/2025--15:10:07

--- kafka.sh
+++ kafka.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "🔍 Checking Kafka..."
 
+$KAFKA_HOME/bin/zookeeper-server-start.sh -daemon $KAFKA_HOME/config/zookeeper.properties
+$KAFKA_HOME/bin/kafka-server-start.sh -daemon $KAFKA_HOME/config/server.properties
+
 # Create a test topic
 kafka-topics.sh --create --topic test-topic --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1 \
   || echo "⚠️ Topic might already exist"
