62c3c4e3d48ebe59a7ef497874f2c7b15cf25de5
neojarvis <techadmin@iamneo.ai>
2025-07-14 08:51:57 +0000

    Auto-save-commit-7/14/2025--2:21:55-PM

---
+++ StreamCheck/socket_example.py
@@ -0,0 +1,14 @@
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+sc = SparkContext("local[2]", "SocketWordCount")
+ssc = StreamingContext(sc, 5)  # 5-second batches
+
+lines = ssc.socketTextStream("localhost", 9999)
+words = lines.flatMap(lambda line: line.split(" "))
+word_counts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+
+word_counts.pprint()
+
+ssc.start()
+ssc.awaitTermination()
