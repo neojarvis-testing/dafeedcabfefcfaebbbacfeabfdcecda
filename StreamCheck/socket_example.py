ebcabcb66451637cc15777050377b5a1fb8fce81
neojarvis <techadmin@iamneo.ai>
2025-07-14 13:11:39 +0000

    Auto-save-commit-7/14/2025--6:41:32-PM

--- StreamCheck/socket_example.py
+++ StreamCheck/socket_example.py
@@ -3,14 +3,20 @@ findspark.init()
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
-sc = SparkContext("local[2]", "SocketWordCount")
-ssc = StreamingContext(sc, 5)  # 5-second batches
+sc = SparkContext("local[2]", "SquareNumbers")
+ssc = StreamingContext(sc, 2)  # 2-second batch interval
 
+# Receive numbers as space-separated string
 lines = ssc.socketTextStream("localhost", 9999)
-words = lines.flatMap(lambda line: line.split(" "))
-word_counts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
 
-word_counts.pprint()
+# Convert strings to integers, then square them
+numbers = lines.flatMap(lambda line: line.split(" ")) \
+               .map(lambda num: int(num)) \
+               .map(lambda x: (x, x**2))
+
+# Print original and squared numbers
+numbers.pprint()
 
 ssc.start()
 ssc.awaitTermination()
+
