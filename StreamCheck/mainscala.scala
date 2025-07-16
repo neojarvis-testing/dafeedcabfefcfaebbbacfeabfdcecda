52e9d9f2ad2e23b02572481bcfcf4290c64810a3
neojarvis <techadmin@iamneo.ai>
2025-07-16 10:35:25 +0000

    Auto-save-commit-7/16/2025--4:05:22-PM

--- StreamCheck/mainscala.scala
+++ StreamCheck/mainscala.scala
@@ -1,9 +1,9 @@
-object ValVarExample {
+object FunctionExample {
+  def calculate(x: Int, y: Int, ans: (Int, Int) => Int): Int = ans(x, y)
+
   def main(args: Array[String]): Unit = {
-    val course = "Scala Programming"
-    var progress = 0
-    progress += 10
-    println(s"Course: $course, Progress: $progress%")
+    val sum = calculate(4, 5, (a, b) => a + b)
+    println(s"Sum = $sum")  // Output: Sum = 9
   }
 }
 
